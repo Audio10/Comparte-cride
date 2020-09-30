f1f4439035803841724653e6ebf8425756da3821
Audio10 <claudiojesus_10@hotmail.com>
2020-09-29 22:42:58 -0500

    6_Finish Update circles views

--- cride/circles/serializers/circles.py
+++ cride/circles/serializers/circles.py
@@ -8,15 +8,43 @@ from cride.circles.models import Circle
 
 
 class CircleModelSerializer(serializers.ModelSerializer):
-    """Circle model serializer."""
+    """Circle model serializer.
+    We specify the members_limit and the is_limited fields because we going to use that,
+    for the data validation.
+    """
+
+    members_limit = serializers.IntegerField(
+        required=False,
+        min_value=10,
+        max_value=32000
+    )
+
+    is_limited = serializers.BooleanField(default=False)
 
     class Meta:
         """Meta class."""
         model = Circle
         fields = (
-            'id', 'name', 'slug_name',
+            'name', 'slug_name',
             'about', 'picture',
             'rides_offered', 'rides_taken',
             'verified', 'is_public',
             'is_limited', 'members_limit'
         )
+
+        # Specifies the read fields.
+        read_only_fields = (
+            'is_public',
+            'verified',
+            'rides_offered',
+            'rides_taken'
+        )
+
+    def validate(self, attrs):
+        """Ensure both members_limit and is_limited are present."""
+        members_limit = attrs.get('members_limit', None)
+        is_limited = attrs.get('is_limited', False)
+        # xor true only for 1-1 and 0-0.
+        if is_limited ^ bool(members_limit):
+            raise serializers.ValidationError('If circle is limited, a member limit must be provided')
+        return attrs
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -5,17 +5,35 @@
     - GenericAPIView: Class that implements a behavior for the CRUD.
     - MIXINS: Are classes that implements a specific behavior for a class that inherit GenericAPIView.
 """
-from rest_framework import viewsets
+
+# Django REST Framework
+from rest_framework import viewsets, mixins
+
+# Permissions
 from rest_framework.permissions import IsAuthenticated
-from cride.circles.models import Circle
+from cride.circles.permissions import IsCircleAdmin
+
+from cride.circles.models import Circle, Membership
 from cride.circles.serializers import CircleModelSerializer
 
 
-class CircleViewSet(viewsets.ModelViewSet):
-    """Circle view set."""
+class CircleViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    """Circle view set.
+
+    The GenericViewSet class inherits from GenericAPIView, and provides the default set of get_object, get_queryset
+    methods and other generic view base behavior, but does not include any actions by default.
+
+    In order to use a GenericViewSet class you'll override the class and either mixin the required mixin classes,
+     or define the action implementations explicitly.
+    """
 
     serializer_class = CircleModelSerializer
-    permission_classes = (IsAuthenticated,)
+    # Is the id that we going to use /platzi-mexico.
+    lookup_field = 'slug_name'
 
     def get_queryset(self):
         """Restrict list to public-only.
@@ -27,3 +45,28 @@ class CircleViewSet(viewsets.ModelViewSet):
             return queryset.filter(is_public=True)
         return queryset
 
+    def get_permissions(self):
+        """Assign permissions based on action.
+
+        To override the permission you need to specify the condition for get other permission,
+        You can create a new permission with the BasePermission class, and appends at the permission.
+
+        We need to create a instance for each permission and return it.
+        """
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
+
+    def perform_create(self, serializer):
+        """Assign circle admin."""
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10
+        )
